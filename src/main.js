bbbb7b3e5c807991f2418ce21dc925764f934af8
Antoni Kepinski <keppi@o2.pl>
2018-07-13 16:01:00 +0200

    v4.4 :rocket:

--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
-const electron = require('electron');
-
-const {app, BrowserWindow} = electron;
-
-let win = null;
+const {app, BrowserWindow} = require('electron');
 
 app.on('ready', () => {
-	win = new BrowserWindow({
+	let win = new BrowserWindow({
 		titleBarStyle: 'hidden',
-		title: 'elcalc',
 		width: 324,
 		height: 295,
 		maximizable: false,
 		minimizable: false,
-		resizable: false
+		resizable: false,
+		darkTheme: true,
+		show: false
 	});
 
 	win.setMenu(null);
 
 	win.loadURL(`file://${__dirname}/index.html`);
+
+	win.once('ready-to-show', () => {
+		win.show();
+	});
 
 	win.on('closed', () => {
 		win = null;
